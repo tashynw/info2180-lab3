9e7d788f55bdd70cadbf6514bdda3c0df27967e4
unknown <wallacetashyn@gmail.com>
2022-10-06 20:21:37 -0500

    Exercise 3 completed - added hover offect on boards

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,8 +4,10 @@ window.addEventListener('DOMContentLoaded',function(){
     boardDivs.map((board)=>{
         board.classList.add('square');
     })
+    
     var lastState = 'O';
     const gameState = [];
+
     function addInput(board){
         if(board.innerText.includes('O')||board.innerText.includes('X')) return;
         if(lastState==='X'){
@@ -21,11 +23,19 @@ window.addEventListener('DOMContentLoaded',function(){
         }
         console.log(gameState)
     }
+    
     var boards = document.getElementsByClassName("square");
     boards=[...boards]
+
     boards.map((board)=>{
         board.addEventListener('click', function(){
             addInput(board)
+        })
+        board.addEventListener('mouseover',function(){
+            board.classList.add('hover')
+        })
+        board.addEventListener('mouseleave',function(){
+            board.classList.remove('hover')
         })
     })
 })
