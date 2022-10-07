67ccca717f6c3fd0fdb15db7a39f619e113d68b2
unknown <wallacetashyn@gmail.com>
2022-10-06 20:09:29 -0500

    Exercise 2 completed - adding X or O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,4 +4,28 @@ window.addEventListener('DOMContentLoaded',function(){
     boardDivs.map((board)=>{
         board.classList.add('square');
     })
+    var lastState = 'O';
+    const gameState = [];
+    function addInput(board){
+        if(board.innerText.includes('O')||board.innerText.includes('X')) return;
+        if(lastState==='X'){
+            board.classList.add('O')
+            board.innerText = 'O'
+            lastState = 'O'
+            gameState.push('O')
+        } else if(lastState==='O'){
+            board.classList.add('X')
+            board.innerText = 'X'
+            lastState = 'X'
+            gameState.push('X')
+        }
+        console.log(gameState)
+    }
+    var boards = document.getElementsByClassName("square");
+    boards=[...boards]
+    boards.map((board)=>{
+        board.addEventListener('click', function(){
+            addInput(board)
+        })
+    })
 })
